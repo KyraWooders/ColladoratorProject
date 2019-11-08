4d6044fd95bb005dd7a3e21c06455e47f5c41b49
s198061 <s198061@LAF-7E868C>
2019-11-08 10:10:44 -0600

    hello

--- ColladoratorProject/Program.cs
+++ ColladoratorProject/Program.cs
@@ -10,6 +10,8 @@ namespace ColladoratorProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, MeatBag");
+            Console.ReadKey();
         }
     }
 }
